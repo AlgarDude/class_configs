6fcae5370bcf9d28f80ecd284bc18a4cdd79605e
AlgarDude <43156286+AlgarDude@users.noreply.github.com>
2024-08-11 19:12:51 -0400

    Update new bardbuffsong check.lua

--- new bardbuffsong check.lua
+++ new bardbuffsong check.lua
@@ -2,16 +2,20 @@
 --These numbers require playtesting and may need to be tweaked, I am considering making them a configurable amount but not sure if that is overly complex for typical usecases.
 ---@param songSpell MQSpell
 ---@return boolean
+--TODO: REFACTOR THIS PUPPY
 function RGMercUtils.BuffSong(songSpell)
     if not songSpell or not songSpell() then return false end
-	
-	local duration = mq.TLO.Me.Song(songSpell.Name()).Duration.TotalSeconds() or 0
+    local me = mq.TLO.Me
 	local casttime = songSpell.MyCastTime.Seconds()
-	local threshold = function(self) if RGMercUtils.GetXTHaterCount() == 0 then return casttime + 12 else return casttime + 6 end
+	local threshold = 6
+	if RGMercUtils.GetXTHaterCount() == 0 then threshold = 12 end
 	
-	local res = RGMercUtils.SongMemed(songSpell) and duration <= threshold
-    RGMercsLogger.log_verbose("BuffSong(%s) => memed(%s), duration(%0.2f) < threshold(%0.2f) --> result(%s)", songSpell.Name(), 
-		RGMercUtils.BoolToColorString(me.Gem(songSpell.Name())() ~= nil), duration, threshold, RGMercUtils.BoolToColorString(res))
+    local res = RGMercUtils.SongMemed(songSpell) and
+        (me.Song(songSpell.Name()).Duration.TotalSeconds() or 0) <= (casttime + threshold)
+    RGMercsLogger.log_verbose("\ayBuffSong(%s) => memed(%s), duration(%0.2f) < reusetime(%0.2f) --> result(%s)",
+        songSpell.Name(),
+        RGMercUtils.BoolToColorString(me.Gem(songSpell.RankName.Name())() ~= nil),
+        me.Song(songSpell.Name()).Duration.TotalSeconds() or 0, casttime + threshold,
+        RGMercUtils.BoolToColorString(res))
     return res
-	end
 end
